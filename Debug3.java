b6d99dd314e6128f0f11905a3b3f22614250719b
Anartz <ikacelarain25@lhusurbil.eus>
2025-10-17 09:12:38 +0200

    akatsak zuzendu

---
+++ Debug3.java
@@ -0,0 +1,74 @@
+//package ariketa2;
+
+import java.util.Scanner;
+
+/**
+ * Akatsak aurkitu
+ */
+public class Debug3 {
+
+	public static void main(String[] args) {
+
+		int aukera = 0;
+		double oper1 = 0;
+		double oper2 = 0;
+		double emaitza = 0;
+		Scanner tekla = new Scanner(System.in);
+
+		do {
+
+			System.out.println(
+					"Operazioak: Batu (1), Kendu (2), Biderkatu (3), Zatitu (4), Gainerakoa (5), Irten (6)");
+			System.out.print("Aukera: ");
+			aukera = tekla.nextInt();
+
+			if (aukera == 6) {
+				System.out.println("Programa AMAIERA");
+			} else {
+
+				System.out.print("Oper1: ");
+				oper1 = tekla.nextDouble();
+
+				System.out.print("Oper2: ");
+				oper2 = tekla.nextDouble();
+
+				if (aukera == 1) {
+					emaitza = oper1 + oper2;
+					System.out.println("Emaitza = " + emaitza);
+				} else if (aukera == 2) {
+					emaitza = oper1 - oper2;
+					/**
+					 * Akatsa: oper1 = oper1 - oper2;
+					 * oper1 ordez emaitza jarri behar da, bestela aurreko ariketako emaitza aterako zaigu
+					 */
+					System.out.println("Emaitza = " + emaitza);
+				} else if (aukera == 4) {
+					if (oper2 == 0) {
+						/**
+						 * Akatsa: oper2 = 1;
+						 * oper2 = 0 denean dago errorea ez oper2 = 1 denean
+						 */
+						System.out.println("0 bidez ezin da zatitu");
+						emaitza = 0; 
+					} else {
+						emaitza = oper1 / oper2;	
+					}
+					System.out.println("Emaitza = " + emaitza);
+				} else if (aukera == 4) {
+					emaitza = oper1 * oper2;
+					System.out.println("Emaitza = " + emaitza);
+				} else if (aukera == 5) {
+					emaitza = oper1 % oper2;
+					/**
+					 * Akatsa: emaitza = oper1 % oper1;
+					 * oper1 % oper1 ez dago ondo
+					 */
+					System.out.println("Emaitza = " + emaitza);
+				}
+				aukera++;
+			}
+
+		} while (aukera != 6);
+		tekla.close();
+	}
+}
